5062a77903248581080426ae6ef794d353b34fc8
Marcel Sussai de Oliveira <marcelsussaideoliveira@hotmail.com>
2018-08-01 01:25:33 -0300

    01/08/2018 - 01:25 - =Adicionando conteudo= - MarcelSussai

--- ref/r01_variaveis/v_05_TruthyFalse/main.js
+++ ref/r01_variaveis/v_05_TruthyFalse/main.js
@@ -1,4 +1,65 @@
 (function(doc, win) {
   'use strict';
-  
+  //‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗
+  // * Teste de verdadeiro ou falso
+  var test1 = 1 === 'b';
+  //――――――――――――――――――――――――――――――――→
+  console.log('________________________________');
+  console.log(test1);
+  console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');
+  //――――――――――――――――――――――――――――――――→
+  function testarTruthyFalse(v) {
+    var test2;
+    if(v) {
+      test2 = true;
+    } else {
+      test2 = false;
+    }
+    //――――――――――――――――→
+    console.log('________________________________');
+    console.log((v === '') ? '\'\'': v + '', '=', test2);
+    console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');    
+  }
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(1);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(0);
+  //――――――――――――――――――――――――――――――――→
+  // * Falsy
+  // ? undefined  = 0
+  // ? null       = -0
+  // ? NaN        = '' ou ""
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse('');
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse('0');
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse('Marcel');
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(undefined);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(null);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse({});
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse([]);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(NaN);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(-0);
+  //――――――――――――――――――――――――――――――――→
+  testarTruthyFalse(2);
+  //――――――――――――――――――――――――――――――――→
+  // * Truthy
+  // ? Todos os outros
+  // ? Interessante, descobrir a representação booleana
+  // ? !!
+  console.log(!true);   // # false
+  console.log(!false);  // # true
+  console.log(!1);      // # false
+  console.log(!!true);  // # true
+  console.log(!!0);     // # false
+  console.log(!!"");    // # false
+  console.log(!!'0');   // # true
+  //˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭
 })(document, window);
