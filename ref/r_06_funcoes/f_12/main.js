b1cdb3e30a5ec9597bb4b503c60bc528ef1ee75d
Marcel Sussai de Oliveira <marcelsussaideoliveira@hotmail.com>
2018-08-12 23:05:23 -0300

    12/08/2018 - 23:05 - =Add mais coisas= - MarcelSussai

--- ref/r_06_funcoes/f_12/main.js
+++ ref/r_06_funcoes/f_12/main.js
@@ -6,6 +6,7 @@
     var n2 = 20;
     console.log('Na função fun01, o primeiro numero é:', n1);
     console.log('Na função fun01, o segundo numero é:', n2);
+    console.log('―――――――――――――――→');
     return n1 + n2;
   }
   //――――――――――――――――→
@@ -16,6 +17,7 @@
     var n1 = 16;
     var n2 = 16;
     console.log('A soma de 16 e 16 é:', sum ? sum() : undefined);
+    console.log('―――――――――――――――→');
     return sum();
   }
   function fun03() {
@@ -26,6 +28,7 @@
     }
     console.log('A soma de 24 e 32 é:', sum());
     console.log('Na função fun03, n1 é:', n1);
+    console.log('―――――――――――――――→');
     return sum();
   }
   //――――――――――――――――――――――――――――――――→
@@ -33,6 +36,48 @@
   console.log(fun01());
   console.log(fun02());
   console.log(fun03());
+  console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');
+  //˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭
+
+  //‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗
+  function calculator(n1, n2) {
+    return function(callback) {
+      return callback(n1, n2);
+    }
+  }
+  //――――――――――――――――――――――――――――――――→
+  var sum = calculator(16, 16);
+  var mul = calculator(8, 8);
+  var div = calculator(128, 8);
+  var sub = calculator(64, 16);
+  var mod = calculator(40, 8);
+  //――――――――――――――――――――――――――――――――→
+  var resSum = sum(function(n1, n2) {
+    return n1 + n2;
+  });
+  //――――――――――――――――→
+  var resMul = mul(function(n1, n2) {
+    return n1 * n2;
+  });
+  //――――――――――――――――→
+  var resDiv = div(function(n1, n2) {
+    return n1 / n2;
+  });
+  //――――――――――――――――→
+  var resSub = sub(function(n1, n2) {
+    return n1 - n2;
+  });
+  //――――――――――――――――→
+  var resMod = mod(function(n1, n2) {
+    return n1 % n2;
+  });
+  //――――――――――――――――――――――――――――――――→
+  console.log('________________________________');
+  console.log('O resultado da soma é:', resSum);
+  console.log('O resultado da multiplicação é:', resMul);
+  console.log('O resultado da divisão é:', resDiv);
+  console.log('O resultado da subtração é:', resSub);
+  console.log('O resultado do módulo é:', resMod);
   console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');
   //˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭
 })(document);
